b0a23f349c90430bc645871153bb743eedd2a089
luciano.schirmer <luciano.schirmer@fulltechpro.com.br>
2021-04-20 16:30:58 -0300

    first commit

---
+++ SOLID/1 - SRP/SRP.Solucao/CPFServices.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID._1___SRP.SRP.Solucao
+{
+    class CPFServices
+    {
+    }
+}
---
+++ SOLID/1 - SRP/SRP.Solucao/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID._1___SRP.SRP.Solucao
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Cpf { get; set; }
+        public DateTime DataCadastro { get; set; }
+
+        public bool IsValid()
+        {
+            return true;
+        }
+    }
+}
